c969953b0b7a49fa20a800fdf7cda40bb30cdcd0
Khaled Bohout <khaledbohout2012@>
2021-05-01 04:08:10 +0200

    added profile views to UI

---
+++ Merca/View/Profile/CustomCells/OrdersTableViewCell/OrdersTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  OrdersTableViewCell.swift
+//  Merca
+//
+//  Created by Khaled Bohout on 01/05/2021.
+//
+
+import UIKit
+
+class OrdersTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
